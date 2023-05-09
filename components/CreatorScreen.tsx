1549537a5bf94ec0006cc2d4393ec91475e0b027
Amber Jain <jainanuamber@gmail.com>
2023-05-09 13:55:05 +0530

    components added

---
+++ components/CreatorScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CreatorScreen = () => {
+  return (
+    <View>
+      <Text>CreatorScreen</Text>
+    </View>
+  )
+}
+
+export default CreatorScreen
