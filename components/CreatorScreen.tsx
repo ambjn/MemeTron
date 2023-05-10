1622d305ddbd3b93b43c88f6f96ed4874506614a
Amber Jain <jainanuamber@gmail.com>
2023-05-10 15:18:06 +0530

    updated with modal

--- components/CreatorScreen.tsx
+++ components/CreatorScreen.tsx
@@ -12,21 +12,27 @@ import {
   FormControl,
   Input,
   Button,
+  Spinner,
+  Modal,
 } from "native-base";
 import MemeSelector from "./MemeSelector";
-import { Meme } from "../hook/useApi";
+import { Meme, useApi } from "../hook/useApi";
 
 interface RouterProps {
   route: RouteProp<{ params: { meme: string } }, "params">;
 }
 const CreatorScreen = (props: RouterProps) => {
+  const { createMeme } = useApi();
+
   const [selected, setSelected] = useState<any>();
   const [selectedName, setSelectedName] = useState<string>();
 
   const [top, setTop] = useState("");
   const [bottom, setBottom] = useState("");
   const [loading, setLoading] = useState(false);
-  const [result, setResult] = useState<any>();
+  const [result, setResult] = useState<any>(null);
+
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const { meme } = props.route.params || { meme: "10-Guy" };
@@ -38,9 +44,54 @@ const CreatorScreen = (props: RouterProps) => {
     setSelected(meme.image);
     setSelectedName(meme.name);
   };
-  const doCreateMeme= () => {}
+  const doCreateMeme = async () => {
+    setLoading(true);
+    const memeBlob = await createMeme(top, bottom, selectedName!);
+    setLoading(false);
+
+    const fileReaderInstance = new FileReader();
+    fileReaderInstance.readAsDataURL(memeBlob.data);
+
+    fileReaderInstance.onload = () => {
+      const base64Data = fileReaderInstance.result;
+      setResult(base64Data);
+      setShowModal(true);
+    };
+  };
+
+    
+
   return (
     <ScrollView>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)} size='lg'>
+        <Modal.Content maxWidth={400}>
+          <Modal.CloseButton />
+          <Modal.Header>Your Meme</Modal.Header>
+          <Modal.Body>
+            <Image
+              source={{ uri: result }}
+              alt='Result'
+              resizeMode='contain'
+              width={"400"}
+              height={"200"}
+            />
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button flex={1} onPress={() => {}}>
+              Download
+            </Button>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+      {loading && (
+        <HStack space={2} m={10} justifyContent='center'>
+          <Spinner accessibilityLabel='Loading meme' color='secondary.500' />
+          <Heading color='secondary.500' fontSize='md'>
+            Loading meme...
+          </Heading>
+        </HStack>
+      )}
       {!loading && (
         <HStack space={2} mb={10} m={4}>
           <VStack space={2} w={"60%"}>
@@ -58,9 +109,12 @@ const CreatorScreen = (props: RouterProps) => {
                   setBottom(text);
                 }}></Input>
             </FormControl>
-            <Button 
-            colorScheme={'secondary'}
-            onPress={() => doCreateMeme()} size={'md'}>Create Meme</Button>
+            <Button
+              colorScheme={"secondary"}
+              onPress={() => doCreateMeme()}
+              size={"md"}>
+              Create Meme
+            </Button>
           </VStack>
           <Center>
             <Image
