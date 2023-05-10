d7100588cbe4025ca61dc725d652b371e20123e6
Amber Jain <jainanuamber@gmail.com>
2023-05-10 14:21:52 +0530

    updated the creator screen

--- components/CreatorScreen.tsx
+++ components/CreatorScreen.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
 import { useState, useEffect } from "react";
 
 import { RouteProp } from "@react-navigation/native";
 import { memes } from "../assets/list";
-import { ScrollView } from "native-base";
+import {
+  HStack,
+  Heading,
+  ScrollView,
+  VStack,
+  Center,
+  Image,
+  FormControl,
+  Input,
+  Button,
+} from "native-base";
 import MemeSelector from "./MemeSelector";
 import { Meme } from "../hook/useApi";
 
@@ -11,8 +20,13 @@ interface RouterProps {
   route: RouteProp<{ params: { meme: string } }, "params">;
 }
 const CreatorScreen = (props: RouterProps) => {
-  const [selected, setSelected] = useState<string>();
+  const [selected, setSelected] = useState<any>();
   const [selectedName, setSelectedName] = useState<string>();
+
+  const [top, setTop] = useState("");
+  const [bottom, setBottom] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState<any>();
 
   useEffect(() => {
     const { meme } = props.route.params || { meme: "10-Guy" };
@@ -24,8 +38,39 @@ const CreatorScreen = (props: RouterProps) => {
     setSelected(meme.image);
     setSelectedName(meme.name);
   };
+  const doCreateMeme= () => {}
   return (
     <ScrollView>
+      {!loading && (
+        <HStack space={2} mb={10} m={4}>
+          <VStack space={2} w={"60%"}>
+            <FormControl>
+              <Input
+                placeholder='Top Text'
+                onChangeText={(text) => {
+                  setTop(text);
+                }}></Input>
+            </FormControl>
+            <FormControl>
+              <Input
+                placeholder='Bottom Text'
+                onChangeText={(text) => {
+                  setBottom(text);
+                }}></Input>
+            </FormControl>
+            <Button 
+            colorScheme={'secondary'}
+            onPress={() => doCreateMeme()} size={'md'}>Create Meme</Button>
+          </VStack>
+          <Center>
+            <Image
+              source={selected}
+              alt='Selected Meme'
+              key={`meme:${selected}`}
+            />
+          </Center>
+        </HStack>
+      )}
       <MemeSelector
         onSelect={(meme) => {
           memeSelected(meme);
